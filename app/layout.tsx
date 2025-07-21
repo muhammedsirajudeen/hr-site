6653d0d7bf15bf8732d1b9e3b494ed20434d1aa6
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-07-21 10:06:26 +0530

    refactor: changed title and description of webpage head

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'onboard tribe',
+  description: 'Hiring site that goes against the norms',
+  generator: 'vercel',
 }
 
 export default function RootLayout({
